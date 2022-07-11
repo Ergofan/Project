a76f34b1a1c1a93ffbc44da51acade6bc3671a6b
Grisha <ergofun68@gmail.com>
2022-07-11 15:59:54 +0200

    first commit

---
+++ js/007.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//alert('Hello');
+
+//const result  = confirm('Are u here?');
+//console.log(result);
+
+//const answer = prompt("Поел кашку?", "DAAA");
+//console.log(answer + 5);
+
+//const answer = +prompt("Поел кашку?", "DAAA");
+//console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('What is ur name?', '');
+answers[1] = prompt('What is ur surname?', '');
+answers[2] = prompt('How old are u?', '');
+
+//document.write(answers);
+
+console.log(typeof(answers));
+console.log(typeof(null));
---
+++ js/dop.js
@@ -0,0 +1,10 @@
+const vehicleBodyWidth = '5000';
+const vehicleBodyLength = '4000'; 
+
+console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ', длина: ' + vehicleBodyLength);
+
+['vruur', 'ecrvrev', 'vvuguegv'].map(a=> )
+
+//Request
+data 
+response
