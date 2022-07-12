66bd36331ea0f65ff2d49a0304d8eb4ff3f50252
Grisha <ergofun68@gmail.com>
2022-07-12 13:29:30 +0200

    lesson 14 if/else

--- js/013.js
+++ js/013.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 let numberOfFilms = +prompt(`Сколько фильмов вы уже посмотрели?`, ``);
 
---
+++ js/014.js
@@ -0,0 +1,7 @@
+"use strict";
+
+if (4 == 4){
+    console.log(`ok`);
+} else {
+    console.log(`not ok`)
+};
