d98f073162b13000dd3f2fcf785a9c5ecdf78852
Grisha <ergofun68@gmail.com>
2022-07-12 13:55:37 +0200

    014 and 015 lessons

--- js/014.js
+++ js/014.js
@@ -1,7 +1,42 @@
 "use strict";
 
-if (4 == 4){
+if (4 == 4) {
     console.log(`ok`);
 } else {
-    console.log(`not ok`)
-};
+    console.log(`not ok`);
+}; 
+
+// if (1) {
+//     console.log(`ok`);
+// } else {
+//     console.log(`not ok`)
+// }; 
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log(`Mistake`);
+// } else if (num > 100) {
+//     console.log(`too much`);
+// } else {
+//     console.log(`pzdc`);
+// } 
+
+// (num === 50) ? console.log(`ok!`) : console.log(`Suka pzdc`);
+
+const num = '50';
+
+switch (num) {
+    case '49':
+        console.log(`Не равно`);
+        break;
+    case '100':
+        console.log(`Не равно`);
+        break;
+    case '50':
+        console.log(`true`);
+        break;
+    default:
+        console.log(`not at this time`);
+        break;
+}
